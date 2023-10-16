4699d6623b79784e8fc40259d1c59df91d282a5a
pikachunou <peterstar.voo@gmail.com>
2023-10-16 11:13:47 -0700

    fixed some bugs

--- checkout.js
+++ checkout.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const addToCartButtons = document.querySelectorAll('.add-to-cart');
-    console.log(addToCartButtons)
+    console.log(addToCartButtons);
     const cartItems = document.getElementById('cart-items');
     console.log(cartItems);
     const cartTotal = document.getElementById('cart-total');
     const cartContents = [];
-
+//this works
     addToCartButtons.forEach(button => {
         button.addEventListener('click', function() {
             const product = this.getAttribute('data-product');
+            console.log(product);
             const price = parseFloat(this.getAttribute('data-price'));
+            console.log(price);
             cartContents.push({ product, price });
             updateCart();
         });
@@ -21,6 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         cartContents.forEach(item => {
             const listItem = document.createElement('li');
+            console.log(listItem)
             listItem.textContent = `${item.product} - $${item.price.toFixed(2)}`;
             cartItems.appendChild(listItem);
             total += item.price;
